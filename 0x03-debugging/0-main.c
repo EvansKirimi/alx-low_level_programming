391194fdc8a2a32bf2eea352ae946894f7e90836
EvansKirimi <ekirimi621@gmail.com>
2023-02-22 03:22:13 -0800

    This file must test that the function positive_or_negative() gives the correct output when given a case of 0.

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+* main - test positive_or_negative() gives the correct output when given 0.
+* Return: 0
+*/
+
+int main(void)
+{
+	int i;
+
+	i = 0;
+	positive_or_negative(i);
+	return (0);
+}
